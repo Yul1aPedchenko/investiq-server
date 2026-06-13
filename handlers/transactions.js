1f705ba98e96e4bf3a0193e7887767a70916d55e
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2026-06-13 14:00:21 +0300

    .

--- handlers/transactions.js
+++ handlers/transactions.js
@@ -1,8 +1,20 @@
+import { User } from "../models/user.js";
+import { Transaction } from "../models/transactions.js";
 import { addTransaction, getTransactions, deleteTransaction } from "../db/db.js";
 
 export const addTransactionHandler = async (req, res) => {
   try {
     const transaction = await addTransaction({ ...req.body, userId: req.user.userId });
+
+    const user = await User.findById(req.user.userId);
+
+    if (transaction.type === "expense") {
+      user.balance -= transaction.amount;
+    } else {
+      user.balance += transaction.amount;
+    }
+
+    await user.save();
 
     res.status(201).json(transaction);
   } catch (error) {
@@ -28,12 +40,25 @@ export const getTransactionsHandler = async (req, res) => {
 
 export const deleteTransactionHandler = async (req, res) => {
   try {
-    await deleteTransaction(req.params.id, req.params.userId);
+    const transaction = await Transaction.findById(req.params.id);
+
+    const user = await User.findById(req.user.userId);
+
+    if (transaction.type === "expense") {
+      user.balance += transaction.amount;
+    } else {
+      user.balance -= transaction.amount;
+    }
+
+    await user.save();
+    await transaction.deleteOne();
 
     res.json({
       message: "Transaction deleted",
     });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({
+      message: error.message,
+    });
   }
 };
